09682ccd23de621099d417f985b851d6084966fd
Junaid Patel <pateljunaid09@gmail.com>
2023-07-16 14:01:17 -0400

    Updates

---
+++ precache-manifest.080c021a1b8199ab7a887c36610287fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c102486211987461eaa2",
+    "url": "/static/css/main.da2d3297.chunk.css"
+  },
+  {
+    "revision": "c102486211987461eaa2",
+    "url": "/static/js/main.708c5447.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c7652bb8ab0a13c4364d",
+    "url": "/static/js/2.6cb2980a.chunk.js"
+  },
+  {
+    "revision": "312107f15665e4a0a3904adda336fa6c",
+    "url": "/index.html"
+  }
+];
