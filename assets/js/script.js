ad65f3276130edd80e605f37033e7fb12d750955
Junaid Patel <pateljunaid09@gmail.com>
2023-07-17 18:12:06 -0400

    fixed scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
     $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
+            scrollTop: $($(this).attr('href')).offset().top - 65,
         }, 500, 'linear')
     });
 
